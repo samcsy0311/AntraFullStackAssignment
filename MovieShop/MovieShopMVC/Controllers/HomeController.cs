02a9a28106b785cf85a828e715831f1603ee2475
samcsy0311 <sammichansyc@gmail.com>
2021-11-29 16:02:02 -0800

    Add MovieShop

---
+++ MovieShop/MovieShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieShopMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShopMVC.Controllers
+{
+     public class HomeController : Controller
+     {
+          //Routing
+          //https://localhost/home/index
+          //by default, it's get
+          [HttpGet]
+          public IActionResult Index()
+          {
+               // Views Folder => Home => Index
+               return View("privacy");
+          }
+
+          //https://localhost/home/privacy
+          [HttpGet]
+          public IActionResult Privacy()
+          {
+               return View();
+          }
+
+          [HttpGet]
+          public IActionResult TopMovies()
+          {
+               return View();
+          }
+
+          [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+          public IActionResult Error()
+          {
+               return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+          }
+     }
+}
