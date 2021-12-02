268adde241e7208202679d02c73f7d1fa7482050
samcsy0311 <sammichansyc@gmail.com>
2021-12-01 22:40:12 -0800

    Added 12/1 contents & Inception page

---
+++ MovieShop(new)/Infrastructure/Services/MovieService.cs
@@ -0,0 +1,37 @@
+﻿using ApplicationCore.Models;
+using ApplicationCore.ServiceInterfaces;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+     public class MovieService : IMovieService
+     {
+          private MovieRepository _movieRepository;
+          
+          public MovieService()
+          {
+               _movieRepository = new MovieRepository();
+          }
+
+          public IEnumerable<MovieCardResponseModel> GetHighestGrossingMovies()
+          {
+               // call my MovieRepository and get the data 
+               var movies = _movieRepository.Get30HighestGrossingMovie();
+
+               // 3rd party Automapper from 
+               // manual way
+               var movieCards = new List<MovieCardResponseModel>();
+               foreach (var movie in movies)
+               {
+                    movieCards.Add (new MovieCardResponseModel { Id = movie.Id, PosterUrl = movie.PosterUrl, Title = movie.Title});
+               }
+
+               return movieCards;
+          }
+     }
+}
--- MovieShop(new)/MovieShopMVC/Controllers/HomeController.cs
+++ MovieShop(new)/MovieShopMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
 using MovieShopMVC.Models;
 using System.Diagnostics;
 
@@ -6,18 +7,27 @@ namespace MovieShopMVC.Controllers
 {
      public class HomeController : Controller
      {
-          private readonly ILogger<HomeController> _logger;
+          private MovieService _movieService;
 
           public HomeController(ILogger<HomeController> logger)
           {
-               _logger = logger;
+               _movieService = new MovieService();
           }
 
+          [HttpGet]
           public IActionResult Index()
           {
-               return View();
+               // Call movie service to get list of movie cards to show in the index view
+               var movieCards = _movieService.GetHighestGrossingMovies();
+
+               // 3 ways to pass data/models from controller action methods to view
+               // 1. Pass the models in the view method (Most important)
+               // 2. ViewBag
+               // 3. ViewData
+               return View(movieCards);
           }
 
+          [HttpGet]
           public IActionResult Privacy()
           {
                return View();
