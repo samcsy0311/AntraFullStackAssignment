40c7fdd84d73376508aaa32c06d1acb13ec5c497
samcsy0311 <sammichansyc@gmail.com>
2021-12-08 10:09:06 -0800

    Login and Register

--- MovieShop(new)/MovieShopMVC/Controllers/AccountController.cs
+++ MovieShop(new)/MovieShopMVC/Controllers/AccountController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MovieShopMVC.Controllers
 {
      public class AccountController : Controller
      {
-          public IActionResult Index()
+         
+          // account/register
+          [HttpGet]
+          public IActionResult Register()
           {
                return View();
           }
+
+          [HttpPost]
+          public IActionResult Register(UserRegisterRequestModel registerRequestModel)
+          {
+               // save the data in database and reutn to login page
+               return View();
+          }
+
+          [HttpGet]
+          public IActionResult Login()
+          {
+               return View();
+          }
+
+          [HttpPost]
+          public IActionResult Login(LoginRequestModel loginRequestModel)
+          {
+               return View();
+          }
+
      }
 }
