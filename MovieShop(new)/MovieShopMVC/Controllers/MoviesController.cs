efa89bb8cc78b63c7638fecf3a8d3465af1764db
samcsy0311 <sammichansyc@gmail.com>
2021-12-08 08:48:29 -0800

    Movie Details Model

---
+++ MovieShop(new)/MovieShopMVC/Controllers/MoviesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShopMVC.Controllers
+{
+     public class MoviesController : Controller
+     {
+        public IActionResult Details(int id)
+          {
+               // call the MovieService wuth DI to get the movie details information
+               return View();
+          }
+     }
+}
